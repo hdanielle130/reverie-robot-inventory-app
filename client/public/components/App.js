793ff1f8d574c196741d9fd4ee9c842e5d8d4edd
Heather Terrell <hdanielle130@gmail.com>
2018-05-22 01:35:58 -0500

    adding other files from local

---
+++ client/public/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hosts from './components/hosts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Express Starter</h1>
+        </header>
+        <Hosts />
+      </div>
+    );
+  }
+}
+
+export default App;
