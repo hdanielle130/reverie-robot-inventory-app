c5f262753cc2d1fc101b54fb6a0acd1beac2d27d
Heather Terrell <hdanielle130@gmail.com>
2018-05-25 17:40:41 -0500

    bug fix

--- models/host.js
+++ models/host.js
@@ -4,41 +4,34 @@ const mongoose = require('mongoose');
 const hostSchema = mongoose.Schema({
     id: {
         type: String,
-        required: true
             //unique id for a specific robot
     },
     date_added: {
         type: String,
-        required: true
             //format as date
             //date added to the inventory system
     },
     first_active: {
         type: String,
-        required: true
             //format as date
             //date the robot was first activated
     },
     current_name: {
         type: String,
-        required: true
             //the name the robot calls itself, first and last
     },
     height: {
         type: Number,
-        required: true
             //format as float
             //height, in inches
     },
     weight: {
         type: Number,
-        required: true
             //format as float
             //weight, in pounds
     },
     intelligence_metric: {
         type: Number,
-        required: true
             //format as integer
             //scale of min 1 to max 20, where 20 is max intelligence
     }
@@ -69,6 +62,7 @@ module.exports.addHost = (host, callback) => {
 module.exports.updateHost = (id, host, options, callback) => {
     var query = { _id: id };
     var update = {
+        id: host.id,
         date_added: host.date_added,
         first_active: host.first_active,
         current_name: host.current_name,
